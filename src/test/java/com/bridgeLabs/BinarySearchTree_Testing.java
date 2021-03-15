0cb2248513a2078f31b90f12bce0b45309a16de0
RachanaBR-jain <br19rachanajain@gmail.com>
2021-03-15 08:00:15 +0530

    Able to create Binary Tress of various size and traversed by using PreOrder method

--- src/test/java/com/bridgeLabs/BinarySearchTree_Testing.java
+++ src/test/java/com/bridgeLabs/BinarySearchTree_Testing.java
@@ -13,15 +13,25 @@ public class BinarySearchTree_Testing {
     public void setUp() {
         tree = new BinarySearchTree<>();
         root = new Node<>(56);
-
     }
 
     @Test
     public void Creating_BST_of3node() {
         tree.insert(root, 30);
         tree.insert(root, 70);
+        tree.insert(root, 22);
+        tree.insert(root, 40);
+        tree.insert(root, 60);
+        tree.insert(root, 95);
+        tree.insert(root, 11);
+        tree.insert(root, 65);
+        tree.insert(root, 3);
+        tree.insert(root, 16);
+        tree.insert(root, 63);
+        tree.insert(root, 67);
+
         tree.preOrder(root);
-        Assertions.assertEquals(3, tree.size(root));
+        Assertions.assertEquals(13, tree.size(root));
     }
 
 }
